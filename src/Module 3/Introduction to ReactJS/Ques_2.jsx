e1bc673736c027965a1b42bff26941a87757df48
Shabarish555 <shabarisha.chitty@gmail.com>
2025-05-19 03:58:40 +0000

    Added year for Ques_2.jsx

--- src/Module 3/Introduction to ReactJS/Ques_2.jsx
+++ src/Module 3/Introduction to ReactJS/Ques_2.jsx
@@ -1,5 +1,13 @@
-// Embedding JavaScript Expressions in JSX
-// Description: Embed a JavaScript expression in JSX to display the current year.
-// Use this component in App.js
+import React from "react";
 
+function Ques_2() {
+  const currentYear = new Date().getFullYear();
 
+  return (
+    <div>
+      <h1>The current year is: {currentYear}</h1>
+    </div>
+  );
+}
+
+export default Ques_2;
