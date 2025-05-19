bca4543efab1e74b1ca5fdf50f9b4b84dd08b9dc
Shabarish555 <shabarisha.chitty@gmail.com>
2025-05-19 04:22:26 +0000

    Added component for Ques_6.jsx

--- src/Module 3/Introduction to ReactJS/Ques_6.jsx
+++ src/Module 3/Introduction to ReactJS/Ques_6.jsx
@@ -1,6 +1,21 @@
-// Conditional Rendering
-// Description: Implement a component that renders different messages based on a passed isLoggedIn prop.
+import React from "react";
+import PropTypes from "prop-types";
 
-// Step 1: Write your code within the file, by the name of component as LoginMessage
-// Step 2: Modify the App.js to use LoginMessage with a boolean prop
+function Ques_6({ isLoggedIn }) {
+  return (
+    <div>
+      {isLoggedIn ? <h2>Welcome back!</h2> : <h2>Please log in.</h2>}
+    </div>
+  );
+}
+
+Ques_6.propTypes = {
+  isLoggedIn: PropTypes.bool.isRequired,
+};
+
+Ques_6.defaultProps = {
+  isLoggedIn: false,
+};
+
+export default Ques_6;
 
