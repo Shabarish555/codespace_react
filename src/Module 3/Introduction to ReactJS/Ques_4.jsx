07d606b51a8d9275c21744f669a89e163726f56c
Shabarish555 <shabarisha.chitty@gmail.com>
2025-05-19 04:11:28 +0000

    Added component for Ques_4.jsx

--- src/Module 3/Introduction to ReactJS/Ques_4.jsx
+++ src/Module 3/Introduction to ReactJS/Ques_4.jsx
@@ -1,15 +1,27 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function Ques_4() {
+function Ques_4({ title, message }) {
   return (
     <div>
-      <h1>Welcome</h1>
-      <p>This is a paragraph</p>
+      <h1>{title}</h1>
+      <p>{message}</p>
     </div>
   );
 }
 
+Ques_4.propTypes = {
+  title: PropTypes.string.isRequired,
+  message: PropTypes.string.isRequired,
+};
+
+Ques_4.defaultProps = {
+  title: "Welcome!",
+  message: "This is a default message from Ques_4 component.",
+};
+
 export default Ques_4;
+
 
 
 
