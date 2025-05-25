6a5b58a7a8f40e0165237fb1a44db23dce6f0ba3
Shabarish555 <shabarisha.chitty@gmail.com>
2025-05-25 13:31:03 +0000

    Added component

--- backend/Module 5/Introduction to Node.js & Express.js/1_hello.js
+++ backend/Module 5/Introduction to Node.js & Express.js/1_hello.js
@@ -5,4 +5,6 @@
 // 1. Write a message "Hello, Node.js" in the console.
 // 2. Open a terminal and navigate to the directory containing hello.js .
 // 3. Run the JavaScript file using Node.js ......
+console.log("Hello, Node.js");
+
 
--- backend/Module 5/Introduction to Node.js & Express.js/2_sample_express_server.js
+++ backend/Module 5/Introduction to Node.js & Express.js/2_sample_express_server.js
@@ -1,2 +1,14 @@
 // Creating a Simple Express Server
 // Description: Write a simple Express server that listens on port 3000.
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Express server!');
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on http://localhost:${port}`);
+});
